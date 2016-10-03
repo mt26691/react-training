4de3fde4aaa05387202734f3a225be2a7aa2820c
Nguyen Manh Tung <nguyenmanhtung848@gmail.com>
2016-10-03 17:52:33 +0700

    set up dev enviroment for redux

--- README.md
+++ README.md
@@ -15,3 +15,14 @@ before performing route trainsitions.
  `State is read-only:` The only way to change the state is to emit an action
 
  `Changes are made with pure functions`
+
+ ## Flux vs Redux, Immutable
+ Smilar: They prescribe that you concentrate your model update logic in a certain layer of your application  
+ Different: Redux does not have dispatcher  
+ Note: Redux team discorages mutate the data  
+
+## Immutable
+[Immutable](https://facebook.github.io/immutable-js/) is JS lib implementing persistent data structures.  
+Developing with immutable data encourages you to think differently about how data flows through your application  
+Immutable collections should be treated as values rather than objects. 
+Note
--- app/Greeter.js
+++ app/Greeter.js
@@ -2,11 +2,27 @@
 import React, {Component} from 'react'
 import config from './config.json';
 import styles from './greeter.css';
+import Immutable from 'immutable'
+class Greeter extends Component {
 
-class Greeter extends Component{
+  constructor(props) {
+    super(props);
+    console.log("dm nhe");
+    let map1 = Immutable.Map({ a: 1, b: 2, c: 3 });
+    let map2 = map1.set('b', 50);
+    map1.get('b'); // 2
+    map2.get('b'); // 50
+    console.log(map1);
+
+    console.log(map1.equals(map2) === true);
+    var map3 = map1.set('b', 50);
+    console.log(map1.equals(map3) === false);
+    console.log(map2.equals(map2));
+  }
+
   render() {
     return (
-      <div class="greeter">
+      <div className="greeter">
         {config.greetText}
       </div>
     );
